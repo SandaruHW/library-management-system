d7a4757664c8b2cf3fe95840a04001a6150b9a26
Sandaru Sathsara <sandarusathsarahw@gmail.com>
2026-03-02 01:02:26 +0530

    Implement initial database context and model for books; configure SQLite in Program.cs

---
+++ LibraryAPI/Models/Book.cs
@@ -0,0 +1,11 @@
+namespace LibraryAPI.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }          // Primary key
+        public string Title { get; set; }    // Book title
+        public string Author { get; set; }   // Book author
+        public string Description { get; set; } // Book description
+    }
+}
+
